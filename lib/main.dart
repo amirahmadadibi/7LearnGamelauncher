9d4a650e52670e1a944d21da9b96d138888e2e76
amirahmadadibi <amirahmadadibi@gmail.com>
2023-05-30 16:59:00 +0330

    S10 E05 design appbar

--- lib/main.dart
+++ lib/main.dart
@@ -11,8 +11,39 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       home: Scaffold(
         backgroundColor: Color(0xff1E1E1E),
+        appBar: AppBar(
+          title: Text('Game Launcher'),
+          backgroundColor: Color(0xff1E1E1E),
+          centerTitle: true,
+          elevation: 0,
+          leading: Container(
+            margin: EdgeInsets.only(left: 10),
+            decoration: BoxDecoration(
+              color: Color(0xff1AFFFFFF),
+              shape: BoxShape.circle,
+            ),
+            child: Center(
+              child: Image.asset('assets/images/icon_like.png'),
+            ),
+          ),
+          actions: [
+            Container(
+              height: 45,
+              width: 45,
+              margin: EdgeInsets.only(top: 2, right: 10),
+              decoration: BoxDecoration(
+                color: Color(0xff1AFFFFFF),
+                shape: BoxShape.circle,
+              ),
+              child: Center(
+                child: Image.asset('assets/images/icon_search.png'),
+              ),
+            )
+          ],
+        ),
         body: ListView.builder(
             itemCount: 20,
             itemBuilder: (context, index) {
